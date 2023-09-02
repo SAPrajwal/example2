4b730f79e5cfa44cb5be991839987c476b29522d
Prajwal <19prajwal2005@gmail.com>
2023-09-02 11:34:51 +0530

    python program added

---
+++ python_progs/arith.py
@@ -0,0 +1,14 @@
+def add(n1,n2):
+ return(n1+n2)
+def sub(n1,n2):
+ return(n1-n2)
+def mul(n1,n2):
+ return(n1*n2)
+def div(n1,n2):
+ return(n1/n2)
+a=int(input("enter num 1 :"))
+b=int(input("enter num 2 :"))
+print("sum=",add(a,b))
+print("sum=",sub(a,b))
+print("sum=",mul(a,b))
+print("sum=",div(a,b))
